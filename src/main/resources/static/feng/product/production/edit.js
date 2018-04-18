5698d0931beaa835903939561a5ab45d73c005e3
wen-li-feng <wen-li.feng@hpe.com>
2018-04-18 15:32:58 +0800

    product and product mapping

---
+++ src/main/resources/static/feng/product/production/edit.js
@@ -0,0 +1,69 @@
+$("#form-production-edit").validate({
+	rules:{
+        itemCode:{
+            required:true,
+        },
+        itemName:{
+            required:true,
+        },
+	},
+	submitHandler:function(form){
+		edit();
+	}
+});
+
+function edit() {
+var productionId  = $("input[name='productionId']").val();
+var itemCode  = $("input[name='itemCode']").val();
+var itemName  = $("input[name='itemName']").val(); 
+var itemNameCn  = $("input[name='itemNameCn']").val(); 
+var productCategory  = $("input[name='productCategory']").val(); 
+var specification  = $("input[name='specification']").val(); 
+var specificationCn  = $("input[name='specificationCn']").val(); 
+var testMethod  = $("input[name='testMethod']").val(); 
+var placeOfOrigin  = $("input[name='placeOfOrigin']").val(); 
+var efficiency  = $("input[name='efficiency']").val(); 
+var safetyStack  = $("input[name='safetyStack']").val(); 
+var createBy  = $("input[name='createBy']").val(); 
+var createTime  = $("input[name='createTime']").val(); 
+var updateBy  = $("input[name='updateBy']").val(); 
+var updateTime  = $("input[name='updateTime']").val(); 
+var remark  = $("input[name='remark']").val(); 
+
+	$.ajax({
+		cache : true,
+		type : "POST",
+		url : "/product/production/save",
+		data : {
+            "productionId": productionId,
+            "itemCode": itemCode,
+            "itemName": itemName, 
+            "itemNameCn": itemNameCn, 
+            "productCategory": productCategory, 
+            "specification": specification, 
+            "specificationCn": specificationCn, 
+            "testMethod": testMethod, 
+            "placeOfOrigin": placeOfOrigin, 
+            "efficiency": efficiency, 
+            "safetyStack": safetyStack, 
+            "createBy": createBy, 
+            "createTime": createTime, 
+            "updateBy": updateBy, 
+            "updateTime": updateTime, 
+            "remark": remark
+		},
+		async : false,
+		error : function(request) {
+			$.modalAlert("System ERROR", "error");
+		},
+		success : function(data) {
+			if (data.code == 0) {
+				parent.layer.msg("Updated successfully, on refreshing ……",{icon:1,time: 500,shade: [0.1,'#fff']},function(){
+					window.parent.location.reload();
+				});
+			} else {
+				$.modalAlert(data.msg, "error");
+			}
+		}
+	});
+}
---
+++ src/main/resources/static/feng/product/production/production.js
@@ -0,0 +1,74 @@
+var prefix = "/product/production"
+
+$(function() {
+	var columns = [{
+            checkbox: true
+        },
+        {
+            field: 'itemCode',
+            title: 'Item Code'
+        },
+        {
+            field: 'itemName',
+            title: 'Item Name'
+        },
+       
+	    {
+            field: 'itemNameCn',
+            title: 'Item Name CN'
+        },
+		
+		{
+            field: 'specification',
+            title: 'specification'
+        },
+		
+		{
+            field: 'remark',
+            title: 'Remark'
+        },
+		
+        {
+            title: 'Action',
+            align: 'center',
+            formatter: function(value, row, index) {
+            	var actions = [];
+				actions.push('<a class="btn btn-primary btn-sm" href="#" title="Edit" mce_href="#" onclick="edit(\'' + row.productionId + '\')"><i class="fa fa-edit"></i></a> ');
+				actions.push('<a class="btn btn-warning btn-sm" href="#" title="Remove" onclick="remove(\'' + row.productionId + '\')"><i class="fa fa-remove"></i></a>');
+				return actions.join('');
+            }
+        }];
+	var url = prefix + "/list";
+	$.initTable(columns, url);
+});
+
+/*新增*/
+function add() {
+    var url = prefix + '/add';
+    layer_show("add Item", url, '800', '600');
+}
+
+/*修改*/
+function edit(productionId) {
+    var url = prefix + '/edit/' + productionId;
+    layer_show("edit item", url, '800', '600');
+}
+
+// 单条删除
+function remove(productionId) {
+	$.modalConfirm("Do you want to remove this production?", function(r) {
+		_ajax(prefix + "/remove/" + productionId, "", "post", r);
+    })
+}
+
+// 批量删除
+function batchRemove() {
+	var rows = $.getSelections("productionId");
+	if (rows.length == 0) {
+		$.modalMsg("Please select the data you want to remove ", "warning");
+		return;
+	}
+	$.modalConfirm("Do you want to remove " + rows.length + " productions?", function(r) {
+		_ajax(prefix + '/batchRemove', { "ids": rows }, "post", r);
+	});
+}
